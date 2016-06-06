970cf53557c98b9baca726f3290bc9b36e255537
Madson Dias <madson.dias@ppgcc.ifce.edu.br>
2016-06-06 16:38:44 -0300

    creating global classifier class

---
+++ classifiers/classifier.m
@@ -0,0 +1,72 @@
+classdef classifier < handle
+    %CLASSIFIER é a classe padrão para criação classificadores
+    
+    properties
+        params % Struct com todos os parâmatros do classificador
+    end
+    
+    methods
+        function obj = classifier(params)
+            obj.params = params;
+        end
+        function errorValue = error(obj, inputTest, outputTest)
+            outputHat = obj.predict(inputTest);
+            confusionMatrix = confusionmat(outputTest, outputHat);
+            errorValue = 1 - (trace(confusionMatrix) / sum(confusionMatrix(:)));
+        end
+        function plot(obj, varargin)
+            if nargin < 2
+                divisions = 100;
+            else
+                divisions = varargin{1};
+            end
+            if size(obj.params.inputTrain,2) ~= 2
+                fprintf('Input training have mor tath two dimensions\n');
+            else
+                xInitial = min(obj.params.inputTrain(:,1));
+                xFinal   = max(obj.params.inputTrain(:,1));
+                yInitial = min(obj.params.inputTrain(:,2));
+                yFinal   = max(obj.params.inputTrain(:,2));
+                
+                xStep    = (xFinal - xInitial)/divisions;
+                yStep    = (yFinal - yInitial)/divisions;
+                
+                [xGrid,yGrid] = meshgrid(xInitial-(10*xStep):xStep:xFinal+(10*xStep),...
+                                         yInitial-(10*yStep):yStep:yFinal+(10*yStep));
+                imageSize = size(xGrid);
+                xyGrid = [xGrid(:) yGrid(:)];
+                
+                outputHat = obj.predict(xyGrid);
+                
+                decisionMap = reshape(outputHat, imageSize);
+                
+                f = figure('Units','inches',...
+                      'Position',[2 2 5 4],...
+                      'PaperPositionMode','auto');
+                set(f,'defaulttextinterpreter','latex');
+                hold on;
+                marker = {'r+','g*','bs','yd'};
+                
+                uniqueLabels = unique(outputHat);
+                
+                
+                if size(obj.params.outputTrain,2) > 1
+                    [~,obj.params.outputTrain] = max(obj.params.outputTrain,[],2);
+                end
+                
+                for i=1:max(uniqueLabels)
+                    inputSamples{i} = obj.params.inputTrain(logical(obj.params.outputTrain==uniqueLabels(i)),:);
+                    %scatter(cl2{i}(:,1),cl2{i}(:,2),30, marker(i,:),'filled');
+                    plot(inputSamples{i}(:,1),inputSamples{i}(:,2),marker{i});
+                end
+                
+                box on;
+                set(gca,'ydir','normal');
+                contour(xGrid, yGrid, decisionMap, 'Color', 'k','LineWidth',1);
+                xlim([xInitial-(10*xStep) xFinal+(10*xStep)]);
+                ylim([yInitial-(10*yStep) yFinal+(10*yStep)]);
+            end
+        end
+    end
+end
+
